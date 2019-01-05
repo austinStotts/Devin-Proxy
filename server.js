79424f88cb10bbda0c986914871dc581ef13cfcb
Devin <devinkrok@gmail.com>
2019-01-05 16:51:21 -0600

    prep for deployment yes sir

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+if(process.env.NODE_ENV === 'production'){
+    require('dotenv').config();
+}
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -7,10 +10,10 @@ app.use(cors());
 
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var serverOne = 'http://localhost:3001',
-    ServerTwo = 'http://localhost:3002',
-    ServerThree = 'http://localhost:3003',
-    ServerFour = 'http://localhost:3004';
+var serverOne = process.env.SERVERONE || 'http://localhost:3001',
+    ServerTwo = process.env.SERVERTWO || 'http://localhost:3002',
+    ServerThree = process.env.SERVERTHREE || 'http://localhost:3003',
+    ServerFour = process.env.SERVERFOUR || 'http://localhost:3004';
  
 app.all("/Comments*", function(req, res) {
     console.log('redirecting to Server1');
